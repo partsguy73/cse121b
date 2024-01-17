a2ffd12aa9bf4751c422b684f085f2a1a13fd938
partsguy73 <lellodrian@gmail.com>
2024-01-17 13:23:13 -0700

    adding week 2

---
+++ scripts/w02-task.js
@@ -0,0 +1,30 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Timothy J. Scott";
+const currentYear = 
+
+
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
